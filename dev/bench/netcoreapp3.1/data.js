1e21044e2c169ec0f4a05874d70bbfe1cc71f20c
github-action-benchmark <github@users.noreply.github.com>
2021-04-27 10:49:13 +0000

    add UnitsNet Benchmarks (netcoreapp3.1) (benchmarkdotnet) benchmark result for e92711c4ae593bba29246f4dab884e8f56f93a71

--- dev/bench/netcoreapp3.1/data.js
+++ dev/bench/netcoreapp3.1/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1619516489918,
+  "lastUpdate": 1619520553193,
   "repoUrl": "https://github.com/lipchev/UnitsNet",
   "entries": {
     "UnitsNet Benchmarks (netcoreapp3.1)": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 27.65159805615743,
             "unit": "ns",
             "range": "± 0.5127055666800506"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "committer": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "distinct": true,
+          "id": "e92711c4ae593bba29246f4dab884e8f56f93a71",
+          "message": "Update benchmark-workflow.yml",
+          "timestamp": "2021-04-27T13:46:21+03:00",
+          "tree_id": "542023e9d52894f7dd4b9c71df544f429ceed5dd",
+          "url": "https://github.com/lipchev/UnitsNet/commit/e92711c4ae593bba29246f4dab884e8f56f93a71"
+        },
+        "date": 1619520547648,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "UnitsNet.Benchmark.UnitsNetBenchmarks.Constructor",
+            "value": 10.92157518863678,
+            "unit": "ns",
+            "range": "± 0.34497699539483395"
+          },
+          {
+            "name": "UnitsNet.Benchmark.UnitsNetBenchmarks.FromMethod",
+            "value": 28.47261627515157,
+            "unit": "ns",
+            "range": "± 0.7581172586545769"
           }
         ]
       }
