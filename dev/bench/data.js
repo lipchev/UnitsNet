82898a10b3b8c108d4afae0429677d057741773a
github-action-benchmark <github@users.noreply.github.com>
2021-04-27 03:59:56 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for abde4aa4ccfe94296b9f45d18c91c0b82c656b1c

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1619495996161,
+  "repoUrl": "https://github.com/lipchev/UnitsNet",
+  "entries": {
+    "Benchmark.Net Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "committer": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "distinct": true,
+          "id": "abde4aa4ccfe94296b9f45d18c91c0b82c656b1c",
+          "message": "fix typo in github-token: ${{ secrets.GITHUB_TOKEN }}",
+          "timestamp": "2021-04-27T06:56:52+03:00",
+          "tree_id": "0d539e01a0608b1de0e2ba9aef9d21e524ce3ede",
+          "url": "https://github.com/lipchev/UnitsNet/commit/abde4aa4ccfe94296b9f45d18c91c0b82c656b1c"
+        },
+        "date": 1619495990994,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "UnitsNet.Benchmark.UnitsNetBenchmarks.Constructor",
+            "value": 9.10436898469925,
+            "unit": "ns",
+            "range": "± 0.03342722244240122"
+          },
+          {
+            "name": "UnitsNet.Benchmark.UnitsNetBenchmarks.Constructor",
+            "value": 8.384888966878256,
+            "unit": "ns",
+            "range": "± 0.04071244321377233"
+          }
+        ]
+      }
+    ]
+  }
+}
