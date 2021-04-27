dbf2d7d950530cfa43478b5d292de6f7489ec5cb
github-action-benchmark <github@users.noreply.github.com>
2021-04-27 10:55:50 +0000

    add UnitsNet Benchmarks (net5.0) (benchmarkdotnet) benchmark result for 880e7b7d17161eb0af6c7ecf47f5f560880f92a3

---
+++ dev/bench/net5.0/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1619520949850,
+  "repoUrl": "https://github.com/lipchev/UnitsNet",
+  "entries": {
+    "UnitsNet Benchmarks (net5.0)": [
+      {
+        "commit": {
+          "author": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "committer": {
+            "email": "lipchev@gmail.com",
+            "name": "lipchev",
+            "username": "lipchev"
+          },
+          "distinct": true,
+          "id": "880e7b7d17161eb0af6c7ecf47f5f560880f92a3",
+          "message": "setup dotnet",
+          "timestamp": "2021-04-27T13:51:44+03:00",
+          "tree_id": "df5c1092780ff832a1326d8185177606b5fa929d",
+          "url": "https://github.com/lipchev/UnitsNet/commit/880e7b7d17161eb0af6c7ecf47f5f560880f92a3"
+        },
+        "date": 1619520947875,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "UnitsNet.Benchmark.UnitsNetBenchmarks.Constructor",
+            "value": 10.933377469579378,
+            "unit": "ns",
+            "range": "± 0.03794122658395441"
+          },
+          {
+            "name": "UnitsNet.Benchmark.UnitsNetBenchmarks.FromMethod",
+            "value": 29.849716822306316,
+            "unit": "ns",
+            "range": "± 0.43407331889681455"
+          }
+        ]
+      }
+    ]
+  }
+}
